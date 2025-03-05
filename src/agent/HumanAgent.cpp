ae096ffa35a73a6eca3986d02d551727c41bccc9
Cannafox <deuterpl@gmail.com>
2025-03-05 02:36:39 +0100

    upda

--- src/agent/HumanAgent.cpp
+++ src/agent/HumanAgent.cpp
@@ -1,17 +1,28 @@
 #include "./HumanAgent.hpp"
 #include <fmt/core.h>
 #include <iostream>
-#include <string>
 #include <stdlib.h>
+#include <string>
 
 HumanAgent::HumanAgent(char playerTile) : playerTile(playerTile) {
   fmt::println("Creating {} agent!", playerTile);
 }
 Coordinates HumanAgent::getAction(Board currentBoard) {
+  fmt::println("Getting action for {}", 'X');
+  currentBoard.print();
+  bool move_is_valid = false;
   std::string playerInput;
-  fmt::print("Enter {} coords: ", this->playerTile);
-  std::cin >> playerInput;
-  Coordinates inputCoords = {(size_t) playerInput[0] - 48, (size_t) playerInput[1] - 48};
+  Coordinates inputCoords;
+  while (!move_is_valid) {
+    fmt::print("Enter {} coords: ", this->playerTile);
+    std::cin >> playerInput;
+    Coordinates inputCoords = {static_cast<size_t>(playerInput[0] - 48),
+                               static_cast<size_t>(playerInput[1] - 48)};
+    if (currentBoard.getTile(inputCoords.x, inputCoords.y) ==
+        TileState::EMPTY) {
+      move_is_valid = true;
+    }
+  }
 
   return inputCoords;
 }
